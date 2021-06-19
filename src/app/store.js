2c270f99035df6ae91e9540b0e21bbcc2648b68a
xodud001 <51165061+xodud001@users.noreply.github.com>
2021-06-19 14:48:02 +0900

    feat : Post 작성 기능 추가
    Slice를 정의하고 이를 Store에 저장한 뒤 dispatch를 이용해서 action을 호출해서 state를 수정

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../feature/posts/postsSlice'
+
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts : postsReducer
+  }
 })
